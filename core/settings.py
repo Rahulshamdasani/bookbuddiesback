977ff2765a95a3ec20c91578115eee0a429ff4c9
Shamdasani, Rahul <rshamdas@iu.edu>
2022-05-03 20:14:00 -0400

    settings

--- core/settings.py
+++ core/settings.py
@@ -13,8 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -82,18 +82,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(
-        default=dj_database_url.config(conn_max_age=600)
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+import dj_database_url
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=dj_database_url.config(conn_max_age=600)
+#     )
+# }
 # Needed for heroku converting to postgres
 db_from_env = dj_database_url.config(conn_max_age=600) 
 DATABASES['default'].update(db_from_env)
